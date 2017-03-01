72ab3585161c3cd3f5c66fc7f6ad126dfc03b877
wuaalb <merlijn.blaauw@gmail.com>
2017-03-01 18:11:29 +0100

    synchronize signatures with latest WORLD

--- pyworld.pyx
+++ pyworld.pyx
@@ -6,13 +6,11 @@ import numpy as np
 cimport numpy as np
 np.import_array()
 
-# Notice: wavread and wavwrite is removed form world.
-# [TODO] It seems that I can't specify fft_size from outside?
 
 cdef extern from "src/world/synthesis.h":
     void Synthesis(const double *f0, 
-        int f0_length, double** spectrogram, 
-        double** aperiodicity, 
+        int f0_length, const double * const *spectrogram,
+        const double * const *aperiodicity, 
         int fft_size, double frame_period, 
         int fs, int y_length, double *y)
 
@@ -21,10 +19,11 @@ cdef extern from "src/world/cheaptrick.h":
     ctypedef struct CheapTrickOption:
         double q1
         double f0_floor
+        int fft_size
 
     int GetFFTSizeForCheapTrick(int fs, const CheapTrickOption *option)
     void InitializeCheapTrickOption(int fs, CheapTrickOption *option)
-    void CheapTrick(const double *x, int x_length, int fs, const double *time_axis,
+    void CheapTrick(const double *x, int x_length, int fs, const double *temporal_positions,
         const double *f0, int f0_length, const CheapTrickOption *option,
         double **spectrogram)
 
@@ -38,26 +37,25 @@ cdef extern from "src/world/dio.h":
         int speed
         double allowed_range
 
-    void InitializeDioOption(DioOption *option)
-    
+    void InitializeDioOption(DioOption *option)    
     int GetSamplesForDIO(int fs, int x_length, double frame_period)
     void Dio(const double *x, int x_length, int fs, const DioOption *option,
-        double *time_axis, double *f0)
+        double *temporal_positions, double *f0)
 
 
 cdef extern from "src/world/d4c.h":
     ctypedef struct D4COption:
-        double dummy
+        double threshold
     
     void InitializeD4COption(D4COption *option)
-    void D4C(const double *x, int x_length, int fs, const double *time_axis,
+    void D4C(const double *x, int x_length, int fs, const double *temporal_positions,
         const double *f0, int f0_length, int fft_size, const D4COption *option,
         double **aperiodicity) 
 
 
 cdef extern from "src/world/stonemask.h":
     void StoneMask(const double *x, int x_length, int fs, 
-        const double *time_axis, const double *f0, int f0_length, 
+        const double *temporal_positions, const double *f0, int f0_length, 
         double *refined_f0)
 
 
@@ -80,28 +78,28 @@ def dio(
     int fs, 
     pyDioOpt):
     ''' Raw Pitch (F0) extractor '''
-    x_length = len(x)
+    cdef int x_length = <int>len(x)
     cdef DioOption dioOption = pyDioOpt.option
     f0_length = GetSamplesForDIO(fs, x_length, dioOption.frame_period)
     cdef np.ndarray[double, ndim=1, mode="c"] f0 = \
         np.zeros(f0_length, dtype = np.dtype('float64'))
-    cdef np.ndarray[double, ndim=1, mode="c"] time_axis = \
+    cdef np.ndarray[double, ndim=1, mode="c"] temporal_positions = \
         np.zeros(f0_length, dtype = np.dtype('float64'))
-    Dio(&x[0], x_length, fs, &dioOption, &time_axis[0], &f0[0])
-    return f0, time_axis
+    Dio(&x[0], x_length, fs, &dioOption, &temporal_positions[0], &f0[0])
+    return f0, temporal_positions
 
 
 def stonemask(
     np.ndarray[double, ndim=1, mode="c"] x not None, 
     np.ndarray[double, ndim=1, mode="c"] f0 not None, 
-    np.ndarray[double, ndim=1, mode="c"] time_axis not None, 
+    np.ndarray[double, ndim=1, mode="c"] temporal_positions not None, 
     int fs):
     ''' Pitch (F0) refinement '''
-    cdef int x_length = len(x)
-    cdef int f0_length = len(f0)
+    cdef int x_length = <int>len(x)
+    cdef int f0_length = <int>len(f0)
     cdef np.ndarray[double, ndim=1, mode="c"] refined_f0 = \
         np.zeros(f0_length, dtype = np.dtype('float64'))
-    StoneMask(&x[0], x_length, fs, &time_axis[0],
+    StoneMask(&x[0], x_length, fs, &temporal_positions[0],
         &f0[0], f0_length, &refined_f0[0])
     return refined_f0
 
@@ -109,13 +107,13 @@ def stonemask(
 def cheaptrick(
     np.ndarray[double, ndim=1, mode="c"] x not None, 
     np.ndarray[double, ndim=1, mode="c"] f0 not None,
-    np.ndarray[double, ndim=1, mode="c"] time_axis not None,
+    np.ndarray[double, ndim=1, mode="c"] temporal_positions not None,
     int fs):
     ''' STRAIGHT spectrum '''
     cdef CheapTrickOption option
     InitializeCheapTrickOption(fs, &option)
-    cdef int x_length = len(x)
-    cdef int f0_length = len(f0)
+    cdef int x_length = <int>len(x)
+    cdef int f0_length = <int>len(f0)
     cdef int fft_size = GetFFTSizeForCheapTrick(fs, &option)
 
     cdef double[:,::1] spectrogram = np.zeros((f0_length, fft_size/2+1))
@@ -125,7 +123,7 @@ def cheaptrick(
     for i in range(f0_length):
         cpp_spectrogram[i] = &spectrogram[i, 0]
 
-    CheapTrick(&x[0], x_length, fs, &time_axis[0],
+    CheapTrick(&x[0], x_length, fs, &temporal_positions[0],
         &f0[0], f0_length, &option, cpp_spectrogram)
     return np.array(spectrogram, dtype=np.float64)
 
@@ -133,11 +131,11 @@ def cheaptrick(
 def d4c(
     np.ndarray[double, ndim=1, mode="c"] x not None, 
     np.ndarray[double, ndim=1, mode="c"] f0 not None,
-    np.ndarray[double, ndim=1, mode="c"] time_axis not None,
+    np.ndarray[double, ndim=1, mode="c"] temporal_positions not None,
     int fs):
     ''' Aperiodicity Estimation '''
-    cdef int x_length = len(x)
-    cdef int f0_length = len(f0)
+    cdef int x_length = <int>len(x)
+    cdef int f0_length = <int>len(f0)
     cdef CheapTrickOption opt
     InitializeCheapTrickOption(fs, &opt)
     cdef int fft_size = GetFFTSizeForCheapTrick(fs, &opt)
@@ -152,7 +150,7 @@ def d4c(
     for i in range(f0_length):
         cpp_aperiodicity[i] = &aperiodicity[i, 0]
 
-    D4C(&x[0], x_length, fs, &time_axis[0],
+    D4C(&x[0], x_length, fs, &temporal_positions[0],
         &f0[0], f0_length, fft_size, &option,
         cpp_aperiodicity) 
     return np.array(aperiodicity, dtype=np.float64)
@@ -165,10 +163,10 @@ def synthesize(
     int fs,
     double frame_period):
     ''' Synthesizer '''
-    cdef int f0_length = len(f0)
+    cdef int f0_length = <int>len(f0)
     y_length = int(f0_length * frame_period * fs / 1000)
     # cdef int fft_size = GetFFTSizeForCheapTrick(fs)
-    cdef int fft_size = (np_spectrogram.shape[1] - 1)*2
+    cdef int fft_size = (<int>np_spectrogram.shape[1] - 1)*2
     cdef np.ndarray[double, ndim=1, mode="c"] y = \
         np.zeros(y_length, dtype = np.dtype('float64'))
 
