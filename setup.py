53fc41ef487762702695815685009581f88162d0
JeremyCCHsu <jeremycchsu@gmail.com>
2017-06-26 15:01:33 +0800

    Bug fixed: remove  arg

--- setup.py
+++ setup.py
@@ -49,8 +49,7 @@ setup(
     name="pyworld",
     ext_modules=ext_modules,
     cmdclass=cmdclass,
-    version='0.2.1',
-    scripts=['update.sh'],
+    version='0.2.1a',
     packages=find_packages(),
     install_requires=[
         'numpy',
@@ -62,7 +61,6 @@ setup(
     author="Pyworld Contributors",
     author_email="jeremycchsu@gmail.com",
     url="https://github.com/JeremyCCHsu/Python-Wrapper-for-World-Vocoder",
-    # download_url="https://github.com/JeremyCCHsu/Python-Wrapper-for-World-Vocoder/archive/v0.2.1.tar.gz",
     description="a Python wrapper for the WORLD vocoder",
     keywords = ['vocoder'],
     classifiers = [],
