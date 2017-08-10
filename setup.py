a024a08639f0148d2232d575add3cd98790016ff
Jeremy Hsu <jeremycchsu@gmail.com>
2017-08-10 10:23:44 -0700

    Build-time dependency solving with 2 hacks (numpy & cython)

--- setup.py
+++ setup.py
@@ -3,37 +3,47 @@ from __future__ import with_statement, print_function, absolute_import
 from setuptools import setup, find_packages, Extension
 from distutils.version import LooseVersion
 
-import numpy as np
+# import numpy as np
 import os
 from glob import glob
 from os.path import join
 
+from setuptools.command.build_ext import build_ext as _build_ext
 
-# This can be loosen probably, though it's fine I think
-min_cython_ver = '0.24.0'
-try:
-    import Cython
-    ver = Cython.__version__
-    _CYTHON_INSTALLED = ver >= LooseVersion(min_cython_ver)
-except ImportError:
-    _CYTHON_INSTALLED = False
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
 
-try:
-    if not _CYTHON_INSTALLED:
-        raise ImportError('No supported version of Cython installed.')
-    from Cython.Distutils import build_ext
-    cython = True
-except ImportError:
-    cython = False
+# # This can be loosen probably, though it's fine I think
+# min_cython_ver = '0.24.0'
+# try:
+#     import Cython
+#     ver = Cython.__version__
+#     _CYTHON_INSTALLED = ver >= LooseVersion(min_cython_ver)
+# except ImportError:
+#     _CYTHON_INSTALLED = False
 
-if cython:
-    ext = '.pyx'
-    cmdclass = {'build_ext': build_ext}
-else:
-    ext = '.cpp'
-    cmdclass = {}
-    if not os.path.exists(join("pyworld", "pyworld" + ext)):
-        raise RuntimeError("Cython is required to generate C++ wrapper")
+# try:
+#     if not _CYTHON_INSTALLED:
+#         raise ImportError('No supported version of Cython installed.')
+#     from Cython.Distutils import build_ext
+#     cython = True
+# except ImportError:
+#     cython = False
+
+# if cython:
+#     ext = '.pyx'
+#     cmdclass = {'build_ext': build_ext}
+# else:
+#     ext = '.cpp'
+#     cmdclass = {}
+#     if not os.path.exists(join("pyworld", "pyworld" + ext)):
+#         raise RuntimeError("Cython is required to generate C++ wrapper")
+
 
 world_src_top = join("lib", "World", "src")
 world_sources = glob(join(world_src_top, "*.cpp"))
@@ -41,23 +51,32 @@ world_sources = glob(join(world_src_top, "*.cpp"))
 ext_modules = [
     Extension(
         name="pyworld.pyworld",
-        include_dirs=[np.get_include(), world_src_top],
+        # include_dirs=[np.get_include(), world_src_top],
+        include_dirs=[world_src_top],
         sources=[join("pyworld", "pyworld.pyx")] + world_sources,
         language="c++")]
 
 setup(
     name="pyworld",
     ext_modules=ext_modules,
-    cmdclass=cmdclass,
-    version='0.2.1c',
+    # cmdclass=cmdclass,
+    
+    cmdclass={'build_ext':build_ext},
+
+    version='0.2.2',
     packages=find_packages(),
+    setup_requires=[
+        'numpy',
+    #     'cython>=0.24.0',
+    ],
     install_requires=[
         'numpy',
-        'cython',
+        'cython>=0.24.0',
     ],
     extras_require={
         'test': ['nose'],
-        'develop': ['cython >= ' + min_cython_ver],
+        # 'develop': ['cython >= ' + min_cython_ver],
+        'sdist': ['numpy', 'cython'],
     },
     author="Pyworld Contributors",
     author_email="jeremycchsu@gmail.com",
