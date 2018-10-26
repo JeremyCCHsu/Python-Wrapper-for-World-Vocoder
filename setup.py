d938c4dd29982b3f4dbbe72324d9fe7c44eb4489
Jeremy Hsu <jeremycchsu@gmail.com>
2018-10-25 20:14:42 -0700

    update ver no.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     name="pyworld",
     ext_modules=ext_modules,
     cmdclass={'build_ext': build_ext},
-    version='0.2.5a',
+    version='0.2.6',
     packages=find_packages(),
     setup_requires=[
         'numpy',
