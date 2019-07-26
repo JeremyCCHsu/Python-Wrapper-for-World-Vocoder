397bd23db5c2b4d5f1f036c03643bb9a9992aa56
JeremyCCHsu <jeremycchsu@gmail.com>
2019-07-26 15:15:00 +0800

    update world

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools.command.build_ext import build_ext as _build_ext
 
 
 DOCLINES = __doc__.split('\n')
-_VERSION = '0.2.8'
+_VERSION = '0.2.9'
 
 class build_ext(_build_ext):
     def finalize_options(self):
